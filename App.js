73c14cc3bfd486d73e1e936eb6efa65655fe818b
jungkeunmo <abc59684495@gmail.com>
2023-03-25 15:04:33 +0900

    img

--- App.js
+++ App.js
@@ -1,1 +1,52 @@
 import React from "react";
+import { View, StyleSheet, Image, Text, ImageBackground } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.view}>
+      <View style={styles.viewBox}>
+        <ImageBackground
+          source={require("homework4/assets/images/img2.jpg")}
+          style={styles.img1}
+        ></ImageBackground>
+      </View>
+
+      <View style={styles.viewBox}>
+        <ImageBackground
+          source={require("homework4/assets/images/img1.jpg")}
+          style={styles.img2}
+        ></ImageBackground>
+      </View>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    flexDirection: "column",
+  },
+
+  viewBox: {
+    flex: 2,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  img1: {
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    resizeMode: "cover",
+  },
+
+  img2: {
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    resizeMode: "cover",
+  },
+});
